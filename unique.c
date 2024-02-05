fa5be91a4750540544354c6659eea838dea83c72
Gal-Rivlin <grivlin@calpoly.edu>
2024-02-05 13:35:28 -0800

    submission

---
+++ unique.c
@@ -0,0 +1,89 @@
+#include <stdio.h>
+#include <ctype.h>
+
+int main(int argc , char *argv[])
+{
+//case 1: we get a file
+    FILE *file;
+    if (argc > 1){
+        file = fopen(argv[1], "r");
+
+        if (file == NULL) {
+        printf("Error opening file");
+        return 1;
+        }
+    }
+    int switcher = 1;
+    char currentline1[100];
+    char currentline2[100];
+    int jitgit = 0;
+
+    while (1){
+        jitgit = 0;
+//the first part of the while loop will be to extract a line 
+//the line will be either from a file, or from a scanf
+//we are alteranating between which lines we are coppying too, so that we compare between them
+        if (argc == 1){
+            if (switcher == 1){
+                if(fgets(currentline1, sizeof(currentline1), stdin) == NULL){
+                    break;
+                }
+                switcher = 2;
+            }
+            else{
+                if(fgets(currentline2, sizeof(currentline2), stdin) == NULL){
+                    break;
+                }
+                switcher = 1;
+            }    
+        }
+        else{
+            if (switcher == 1){
+                if(fgets(currentline1, sizeof(currentline1), file) == NULL){
+                    break;
+                }
+                switcher = 2;
+            }
+            else{
+                if(fgets(currentline2, sizeof(currentline2), file) == NULL){
+                    break;
+                }
+                switcher = 1;
+            }
+        }
+
+//now the third part will be to read and check if there's a difference
+        int unique = 0;
+        int line_char_ct = 0;
+        while (currentline1[line_char_ct] != '\0' || currentline2[line_char_ct] != '\0'){
+            if (currentline1[line_char_ct] != currentline2[line_char_ct]){
+                if ((currentline1[line_char_ct] == '\0' && currentline2[line_char_ct] == '\n' )||
+                    (currentline1[line_char_ct] == '\n' && currentline2[line_char_ct] == '\0')){
+                        jitgit = 1;
+                        break;
+                    }
+                unique = 1;
+                break;
+            }
+            line_char_ct++;
+
+        }
+
+    
+// fourth part we are checking if it's unique, and if it is we print it out
+//remember we switched the sign of "switcher", so it's actually opposite signs now
+        if (unique){
+            if (switcher == 2){
+                printf("%s" , currentline1);
+            }
+            if (switcher == 1){
+                printf("%s" , currentline2);
+            }
+            jitgit = 1;
+        }   
+    }
+    if (jitgit){
+        printf("\n");
+    }
+    return 0;
+}
---
+++ word_count.c
@@ -0,0 +1,73 @@
+#include <stdio.h>
+#include <ctype.h>
+
+
+int main(int argc , char *argv[])
+{
+//case 1: we get a file
+    FILE *file;
+    if (argc > 1){
+        file = fopen(argv[1], "r");
+
+        if (file == NULL) {
+        perror("Error opening file");
+        return 1;
+        }
+    }
+    int running = 1;
+
+    int line_ct = 0;
+    int word_ct = 0;
+    int char_ct = 0;
+
+    while (running){
+//the first part of the while loop will be to extract a line 
+//the line will be either from a file, or from a scanf
+        char currentline[100];
+        currentline[0] = '\0';
+
+        if (argc == 1){
+            fgets(currentline, sizeof(currentline), stdin);
+        }
+        else{
+            fgets(currentline, sizeof(currentline), file);
+        }
+
+//the second part will be to check if we are done: we do this by checking if the first char is \0
+        if (currentline[0] == '\0'){
+            running = 0;
+        }
+
+//now the third part will be to read the line and add to our counters
+
+        if (running){
+            line_ct++;
+            int line_char_ct = 0;
+            int new_word = 1;
+            while (currentline[line_char_ct] != '\0'){
+                char_ct++;
+                if (new_word && !isspace(currentline[line_char_ct])){
+                    new_word = 0;
+                    word_ct++;
+                }
+                else if (!new_word && isspace(currentline[line_char_ct])){
+                    new_word = 1;
+                }
+                else if (currentline[line_char_ct] == '\n'){
+                    line_char_ct++;
+                }
+                
+
+            }
+
+
+
+        }
+
+
+    }
+
+    printf("%d    %d    %d  %s\n" , line_ct , word_ct , char_ct, argv[1]);
+
+    return 0;
+}
